dbfb1b3f8b80442b537afbad5eb217bc21099cda
Brecht-Vandekerkhove <157580961+Brecht-Vandekerkhove@users.noreply.github.com>
2024-03-05 11:38:57 +0100

    Test

--- database/migrations/2024_02_29_153922_create_chirps_table.php
+++ database/migrations/2024_02_29_153922_create_chirps_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('chirps', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('message');
             $table->timestamps();
         });
     }
@@ -23,5 +25,21 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('chirps');
+
+
+
     }
+    App\Models\Chirp::all();
+    => Illuminate\Database\Eloquent\Collection {#4512
+        all: [
+          App\Models\Chirp {#4514
+            id: 1,
+            user_id: 1,
+            message: "I'm building Chirper with Laravel!",
+            created_at: "2022-08-24 13:37:00",
+            updated_at: "2022-08-24 13:37:00",
+          },
+        ],
+      }
+
 };
